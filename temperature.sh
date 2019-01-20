7fb336b0a12140dd24062f57abae14c3cf1290d6
YYYYusuke <nakajo@west.sd.keio.ac.jp>
2019-01-20 09:36:17 -0500

    for oakilo.

--- temperature.sh
+++ temperature.sh
@@ -3,9 +3,9 @@
 filename=`date +%m%d%H%M`_temperature.txt
 touch $filename
 echo time>$filename
-/home/minato/Documents/master/ilo4/auto_oak.expect |grep DeviceID|awk -F '=' '{print $2}' >>$filename
+./auto_oak.expect |grep DeviceID|awk -F '=' '{print $2}' >>$filename
 while :
 	do
 		date +%H:%M:%S.%3N >>$filename
-		/home/minato/Documents/master/ilo4/auto.expect |grep Curr|awk -F '=' '{print $2}' >>$filename
+		./auto_oak.expect |grep Curr|awk -F '=' '{print $2}' >>$filename
 	done
